376f325f66bc652899a8e1178a11c9bd1c08b441
Nick <njaginicholas@gmail.com>
2024-05-25 11:32:25 +0300

    update CalendarModal.tsx

--- components/modals/CalendarModal.tsx
+++ components/modals/CalendarModal.tsx
@@ -15,8 +15,16 @@ const CalendarModal = () => {
       <Button size="sm" onPress={onOpen} variant="light">
         <Calendar />
       </Button>
-      <Modal id="" className="w-full sm:max-w-none mx-auto h-full sm:max-h-fit sm:m-0 sm:rounded-none"  scrollBehavior={scrollBehavior as "inside"} isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
-        <ModalContent >
+      <Modal
+        id=""
+        className="w-full sm:max-w-none mx-auto h-full sm:max-h-fit sm:m-0 sm:rounded-none"
+        // scrollBehavior={scrollBehavior as "inside"}
+        isOpen={isOpen}
+        isDismissable={false}
+        onOpenChange={onOpenChange}
+        placement="top-center"
+      >
+        <ModalContent>
           {(onClose) => (
             <>
               <ModalBody className="w-full md:max-w-7xl mx-auto">
@@ -27,6 +35,6 @@ const CalendarModal = () => {
         </ModalContent>
       </Modal>
     </>
-  )
+  );
 }
 export default CalendarModal
